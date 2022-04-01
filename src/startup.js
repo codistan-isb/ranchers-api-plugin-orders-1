6ee57bb6f8aa065121d0fd89eaa295decb553202
Usama Zulfiqar <11besehzulfiqar@seecs.edu.pk>
2022-04-01 17:07:50 +0500

    new order notifcation

--- src/startup.js
+++ src/startup.js
@@ -1,4 +1,6 @@
 import sendOrderEmail from "./util/sendOrderEmail.js";
+import createNotification from "./util/createNotification.js";
+import getProductbyId from "./util/getProductbyId.js";
 
 /**
  * @summary Called on startup
@@ -6,8 +8,23 @@ import sendOrderEmail from "./util/sendOrderEmail.js";
  * @param {Object} context.collections Map of MongoDB collections
  * @returns {undefined}
  */
-export default function ordersStartup(context) {
+export default  function ordersStartup(context) {
   const { appEvents } = context;
 
   appEvents.on("afterOrderCreate", ({ order }) => sendOrderEmail(context, order));
+  appEvents.on("afterOrderCreate", async ({ order ,createdBy}) =>{
+    
+  let productPurchased= await getProductbyId(context,{productId:order?.shipping[0]?.items[0]?.variantId})
+  
+  createNotification(context,{
+    details: null,
+    from: createdBy,
+    hasDetails: false,
+    message: `You have a new order of ${productPurchased.title}`,
+    status: "unread",
+    to: productPurchased?.uploadedBy?.userId,
+    type: "newOrder",
+    url: `/en/profile/address`})
+  
+  });
 }
