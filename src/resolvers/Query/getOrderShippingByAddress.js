c78638d51b650fdb45e714e0997b256957dfa585
Usama Zulfiqar <11besehzulfiqar@seecs.edu.pk>
2023-06-06 13:25:26 +0500

    add: get shipping fullfilment methode by address

--- src/resolvers/Query/getOrderShippingByAddress.js
+++ src/resolvers/Query/getOrderShippingByAddress.js
@@ -1,7 +1,14 @@
 import ReactionError from "@reactioncommerce/reaction-error";
 import encodeOpaqueId from "@reactioncommerce/api-utils/encodeOpaqueId.js";
-const encodeFulfillmentGroupOpaqueId = encodeOpaqueId("reaction/fulfillmentMethod");
 
+//encode fulfillment id 
+const encodeFulfillmentGroupOpaqueId = encodeOpaqueId("reaction/fulfillmentMethod");
+//check user city, return true if islamabad 
+function isSameCity(city) {
+  const pattern = /^(isd|islamabad|rwp|rawalpindi|ict)$/;
+  const isValid = pattern.test(city.toLowerCase());
+  return isValid;
+}
 /**
  * @name getOrderShippingByAddress
  * @method
@@ -14,18 +21,50 @@ const encodeFulfillmentGroupOpaqueId = encodeOpaqueId("reaction/fulfillmentMetho
  * @param {String} [params.token] - Anonymous order token
  * @returns {Promise<Object>|undefined} - An Order document, if one is found
  */
-export default async function getOrderShippingByAddress(parnet, { input } = {},context) {
-  console.log("input",input);
-  const {address, city}=input;
+
+export default async function getOrderShippingByAddress(parnet, { input } = {}, context) {
+  console.log("input", input);
+  const { address, city, amount } = input;
   if (!address || !city) {
     throw new ReactionError("invalid-param", "You must provide address and city arguments");
   }
-  const {collections}=context;
-  const {Shipping}=collections;
-const getShippingMethod=await Shipping.find({}).toArray();
-console.log(getShippingMethod[0]?.methods[0]["_id"]);
-const selectedFullfilmentId=await encodeFulfillmentGroupOpaqueId(getShippingMethod[0]?.methods[0]["_id"])
-console.log("selectedFullfilmentId",selectedFullfilmentId)
-  return selectedFullfilmentId;
- 
+  const { collections } = context;
+  const { Shipping } = collections;
+  const ShippingMethodsObj = await Shipping.find({}).toArray();
+  if (ShippingMethodsObj) {
+    const ShippingMethods = ShippingMethodsObj[0];
+    let selectedFullfilmentId = null;
+    if (parseInt(amount) >= 3000) {
+      const selectedMethodId = ShippingMethods?.methods.filter(item => item.group.toLowerCase() == "free");
+      selectedFullfilmentId = await encodeFulfillmentGroupOpaqueId(selectedMethodId[0]?._id);
+      
+      return {...selectedMethodId[0],
+        // _id:selectedFullfilmentId
+      };
+    }
+    else if (isSameCity(city)==true) {
+      const selectedMethodId = ShippingMethods?.methods.filter(item => item.name.toLowerCase() == "same-city");
+      selectedFullfilmentId = await encodeFulfillmentGroupOpaqueId(selectedMethodId[0]?._id);
+      
+       return {...selectedMethodId[0],
+        // _id:selectedFullfilmentId
+      };
+
+    }
+    else {
+      const selectedMethodId = ShippingMethods?.methods.filter(item => item.name.toLowerCase() == "other-city");
+      selectedFullfilmentId = await encodeFulfillmentGroupOpaqueId(selectedMethodId[0]?._id);
+      
+      return {...selectedMethodId[0],
+        // _id:selectedFullfilmentId
+      };
+    }
+
+
+  }
+  else {
+    throw new ReactionError("", "No Fulfillment methods found, create a fulfillment methode and try again");
+
+  }
+
 }
