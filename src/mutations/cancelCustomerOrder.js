e9a40bcee3acd6bff7a5e14cd49faca5fb121a56
shoaibfarooq3 <shoaibfarooq3@gmail.com>
2023-08-07 20:22:11 +0500

    update: cancelCustomerOrder with push notification

--- src/mutations/cancelCustomerOrder.js
+++ src/mutations/cancelCustomerOrder.js
@@ -4,30 +4,43 @@ import Random from "@reactioncommerce/random";
 export default async function cancelCustomerOrder(context, input) {
   //   inputSchema.validate(input);
   //   console.log("input ", input);
-  const { orderID, cancelOrderReason } = input;
+  let { orderID, cancelOrderReason } = input;
 
-  const { accountId, appEvents, collections, userId } = context;
-  const { Orders } = collections;
+  let { accountId, appEvents, collections, userId } = context;
+  let { Orders } = collections;
   //   console.log("orderID ", orderID);
   // First verify that this order actually exists
-  const order = await Orders.findOne({ _id: orderID });
+  let order = await Orders.findOne({ _id: orderID });
   if (!order) throw new ReactionError("not-found", "Order not found");
-
-  const modifier = {
+  let rejectionReason = cancelOrderReason;
+  //   const userId = CurrentRiderID;
+  let modifier = {
     $set: {
       "workflow.status": "canceled",
-      rejectionReason: cancelOrderReason,
+      rejectionReason,
       updatedAt: new Date(),
     },
   };
-
-  const { modifiedCount, value: updatedOrder } = await Orders.findOneAndUpdate(
+  let appTypeCustomer = "customer";
+  let CustomerAccountID = order?.accountId;
+  let message = `Your order is canceled and reason is ${rejectionReason}`;
+  let { modifiedCount, value: updatedOrder } = await Orders.findOneAndUpdate(
     { _id: orderID },
     modifier,
     { returnOriginal: false }
   );
-  console.log("updatedOrder", updatedOrder);
+  //   console.log("updatedOrder", updatedOrder);
   if (modifiedCount === 0 || !updatedOrder)
     throw new ReactionError("server-error", "Unable to update order");
+  let paymentIntentClientSecret1 =
+    context.mutations.oneSignalCreateNotification(context, {
+      message,
+      id: CustomerAccountID,
+      appType: appTypeCustomer,
+      userId: CustomerAccountID,
+      orderID: orderID,
+    });
+  console.log("paymentIntentClientSecret1 ", paymentIntentClientSecret1);
+
   return { order: updatedOrder };
 }
