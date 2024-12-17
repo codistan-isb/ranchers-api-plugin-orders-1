61d3e62d74e064bbaf3f99d0253f06d0ab247067
harisabbasi1 <haris.abbasi1@codistan.org>
2024-12-17 19:20:55 +0500

    vaidations added

--- src/util/easyPaisaPayment.js
+++ src/util/easyPaisaPayment.js
@@ -17,11 +17,11 @@ export default async function doEasyPaisaPayment(
     emailAddress)
     let data = JSON.stringify({
         "orderId": orderId||"abc123",
-        "storeId": storeId||"397634",
+        "storeId": storeId||process.env.EASYPAISASTOREID,
         "transactionAmount": transactionAmount||"1.00",
         "transactionType": transactionType||"MA",
-        "mobileAccountNo": mobileAccountNo||"03364445126",
-        "emailAddress": emailAddress||"usama.sama@gmail.com"
+        "mobileAccountNo": mobileAccountNo,
+        "emailAddress": emailAddress
       });
       console.log("data ",data)
       let config = {
@@ -29,7 +29,7 @@ export default async function doEasyPaisaPayment(
         maxBodyLength: Infinity,
         url: 'https://easypay.easypaisa.com.pk/easypay-service/rest/v4/initiate-ma-transaction',
         headers: { 
-          'Credentials': 'UmFuY2hlcnNjYWZlOjU1MDU4ZjY2NWQ1ODc5MDYxZDg1NDBlZTEyMzUzYjZh', 
+          'Credentials': process.env.EASYPAISACREDENTIALS, 
           'Content-Type': 'application/json', 
           'Cookie': 'f5avraaaaaaaaaaaaaaaa_session_=HLMGKINDPKOJPKHOFEJMHOFEAPLIEDNBIFMADPFDMFDBKJMAKJPJNJDHBOMIFKDDNPJDBLJAELGDJNPNGKFAHGPIBDFNHNIPGPKKOPNLFAMPKIHGHMCEGFLIEIAJCECG; TS01f2a187=011c1a8db659dbb038859aba2f36856f49c5f911252f3f7aa8f963e626dc41fc3e6bf3995de8df0b30f8604415537bd0b25978dbb226ae694d9bffc43ef74feae68d5e4754; f5avraaaaaaaaaaaaaaaa_session_=DKJDJDGCAKOILNAIFIMBBPDNGBEELNCHEKICBGFHGICKPLHBPDALCJIMBPPODMIGGPFDJNOKBBCECALJIFOADBPJBCIJKCAHMDAOJGFAOIKNDBIADHNFAFCCEBAIOJHP; TS01f2a187=011c1a8db62ea093b93cb0f4b4084e8c77949764178c31e2d7b022ef68219ae814908501b769506889b7f1a1e288e8efe804be3e6a042c4ca76066fb41057d939d4f9f4cf4'
         },
